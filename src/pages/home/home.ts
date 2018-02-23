c26701a8de558061b15b89de00188075b80e33b8
Austin Andrews <a5mercuryhu@gmail.com>
2018-02-22 20:16:41 -0600

    Upload presentation code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,86 @@
 import {Component} from '@angular/core';
-import {NavController} from "ionic-angular";
+import {AlertController, NavController} from "ionic-angular";
+import {TasksService} from "../../services/tasks.service";
 
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
 })
 export class HomePage {
-    
+
+    tasks: Array<any> = [];
+
     constructor(
         public navCtrl: NavController,
+        private tasksSvc: TasksService,
+        public alertCtrl: AlertController
     ) {
-        //
+        this.getTasks();
+    }
+
+    getTasks() {
+        this.tasksSvc.get()
+            .then((response) => {
+                this.tasks = response.tasks.map((t) => {
+                    return {
+                        id: t.id,
+                        label: t.label,
+                        completed: t.completed != null
+                    };
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    addTask() {
+        let prompt = this.alertCtrl.create({
+            title: 'Add New Task',
+            message: 'Enter a label from your new task.',
+            inputs: [
+                {
+                    name: 'label',
+                    placeholder: 'Label'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('cancelled');
+                    }
+                },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        this.tasksSvc.create({
+                            label: data.label
+                        })
+                            .then((response) => {
+                                this.getTasks();
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                            });
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
+
+    complete(task) {
+        this.tasksSvc.complete({
+            task_id: task.id
+        })
+            .then((r) => {
+                setTimeout(() => {
+                    this.getTasks();
+                }, 500);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
     }
 }
--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {BaseHttpService} from "./base-http.service";
 import {Http} from "@angular/http";
+import {EnvironmentConstants} from "../constants/environment.constants";
 
 @Injectable()
 export class TasksService extends BaseHttpService {
@@ -8,4 +9,23 @@ export class TasksService extends BaseHttpService {
     constructor(private http: Http) {
         super();
     }
+
+    get(): Promise<any> {
+        return this.http.get(EnvironmentConstants.api + '/tasks')
+            .map(this.parseResponse)
+            .toPromise();
+    }
+
+    create(payload: {label: string}) {
+        return this.http.post(EnvironmentConstants.api + '/tasks/create', payload)
+            .map(this.parseResponse)
+            .toPromise();
+    }
+
+    complete(payload: {task_id: number}) {
+        return this.http.post(EnvironmentConstants.api + '/tasks/complete', payload)
+            .map(this.parseResponse)
+            .toPromise();
+    }
+
 }
